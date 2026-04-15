1df73147503a3e9f0fe6084a6d14ab6d1652b837
Anvar Xusniddinov <anvarxsuniddinov7997@gmail.com>
2026-04-15 19:47:38 +0500

    django users

--- app/views.py
+++ app/views.py
@@ -9,6 +9,15 @@
 #     return render(request, 'home.html')
 
 from django.shortcuts import render
-
+from . import models
 def home(request):
     return render(request, 'index.html')
+def users(request):
+    # users = [
+    #     {'ism': 'Saidakbar', 'familiya': 'Afzalxonov', 'yosh': 12},
+    #     {'ism': 'Azizbek', 'familiya': 'Abduraximov', 'yosh': 15},
+    #     {'ism': 'Islom', 'familiya': 'Toshboltayev', 'yosh': 14},
+    # ]
+
+    users = models.User.objects.all()
+    return render(request, 'index.html', context={'users': users})
